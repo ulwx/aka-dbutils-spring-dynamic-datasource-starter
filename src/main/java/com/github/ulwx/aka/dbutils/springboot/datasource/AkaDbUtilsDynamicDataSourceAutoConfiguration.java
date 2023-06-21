247062b2b56e518250efc8066d2281444f422184
ulwx <39908619@qq.com>
2023-06-21 10:26:15 +0800

    更新

--- src/main/java/com/github/ulwx/aka/dbutils/springboot/datasource/AkaDbUtilsDynamicDataSourceAutoConfiguration.java
+++ src/main/java/com/github/ulwx/aka/dbutils/springboot/datasource/AkaDbUtilsDynamicDataSourceAutoConfiguration.java
@@ -12,10 +12,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @EnableConfigurationProperties(AkaDbUtilsDynamicDataSourceProperties.class)
-@Configuration
+//@Configuration("com.github.ulwx.aka.dbutils.springboot.datasource.AkaDbUtilsDynamicDataSourceAutoConfiguration")
 public class AkaDbUtilsDynamicDataSourceAutoConfiguration {
     private static final Logger log = LoggerFactory.getLogger(AkaDbUtilsDynamicDataSourceAutoConfiguration.class);
     private final AkaDbUtilsDynamicDataSourceProperties properties;
